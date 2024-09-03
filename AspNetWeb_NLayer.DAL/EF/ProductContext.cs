e178c8a5acba30bda105a56ad3fc7ecb74929ac4
Kostiantyn Polishko <polxs_wp31@student.itstep.org>
2024-09-03 06:40:36 +0300

    2 was created Class Library with DAL architecture for entity ProductItem

---
+++ AspNetWeb_NLayer.DAL/EF/ProductContext.cs
@@ -0,0 +1,22 @@
+﻿using AspNetWeb_NLayer.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AspNetWeb_NLayer.DAL.EF
+{
+    public class ProductContext : DbContext
+    {
+        public DbSet<ProductItem> productItems { get; set; } = null!;
+
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ProductItemConfig());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ AspNetWeb_NLayer.DAL/EF/ProductItemConfig.cs
@@ -0,0 +1,15 @@
+﻿using AspNetWeb_NLayer.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AspNetWeb_NLayer.DAL.EF
+{
+    public class ProductItemConfig : IEntityTypeConfiguration<ProductItem>
+    {
+        public void Configure(EntityTypeBuilder<ProductItem> builder)
+        {
+            builder.ToTable("products").HasKey(c => c.id);
+            builder.HasIndex(c => c.name).IsUnique();
+        }
+    }
+}
