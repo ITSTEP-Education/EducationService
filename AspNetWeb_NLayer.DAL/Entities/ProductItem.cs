581454fa8c5ef64b9c93e6923ac87e4f91b9b9be
Kostiantyn Polishko <polxs_wp31@student.itstep.org>
2024-09-03 06:40:36 +0300

    2 was created Class Library with DAL architecture for entity ProductItem

---
+++ AspNetWeb_NLayer.DAL/Entities/ProductItem.cs
@@ -0,0 +1,16 @@
+﻿
+namespace AspNetWeb_NLayer.DAL.Entities
+{
+    public class ProductItem
+    {
+        public int id {  get; set; }
+        public string name { get; set; } = null!;
+        public string description { get; set; } = null!;
+
+        public ProductItem(string name, string description)
+        {
+            this.name = name.ToLower();
+            this.description = description.ToLower();
+        }
+    }
+}
