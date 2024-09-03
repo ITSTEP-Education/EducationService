f0efd605b3b84a517073a1d2e8388553d0c0353c
Kostiantyn Polishko <167745085+KostiantynPolishko2@users.noreply.github.com>
2024-09-03 06:55:36 +0300

    2 was created Class Library with DAL architecture for entity ProductItem (#1)

---
+++ AspNetWeb_NLayer.DAL/Repositories/ProductItemRepository.cs
@@ -0,0 +1,20 @@
+﻿using AspNetWeb_NLayer.DAL.EF;
+using AspNetWeb_NLayer.DAL.Entities;
+using AspNetWeb_NLayer.DAL.Interfaces;
+
+namespace AspNetWeb_NLayer.DAL.Repositories
+{
+    public class ProductItemRepository : IRepository<ProductItem>
+    {
+        public ProductContext context;
+
+        public ProductItemRepository(ProductContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<ProductItem> getAllItems() => context.productItems;
+
+        public ProductItem? getItem (string? name) => context.productItems.FirstOrDefault(x => x.Equals(name!.ToLower()));
+    }
+}
