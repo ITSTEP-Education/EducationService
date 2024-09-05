bee83b120783d9c50ca63811497236959aa5945c
Kostiantyn Polishko <polxs_wp31@student.itstep.org>
2024-09-05 04:55:23 +0300

    3 was extended properties of ProductItem, created Class Library BLL
    BLL - Bussines Logic Layer

---
+++ AspNetWeb_NLayer.BLL/DTO/PoductItemDto.cs
@@ -0,0 +1,20 @@
+﻿using AspNetWeb_NLayer.DAL.Entities;
+
+namespace AspNetWeb_NLayer.BLL.DTO
+{
+    public class PoductItemDto
+    {
+        public int id {  get; set; }
+        public string name { get; set; } = null!;
+        public string typeEngeeniring { get; set; } = null!;
+
+        public PoductItemDto() { }
+
+        public PoductItemDto(ProductItem productItem)
+        {
+            this.id = productItem.id;
+            this.name = productItem.name;
+            this.typeEngeeniring = productItem.typeEngeeniring;
+        }
+    }
+}
