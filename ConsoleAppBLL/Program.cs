b55955f6cd8046f20040caae7e79a02e9add2e64
Kostiantyn Polishko <167745085+KostiantynPolishko2@users.noreply.github.com>
2024-09-09 20:53:18 +0300

    release v1.0.0 PresenceLayer (ProductController)  (#4)
    * 2 was created default project AspNetWeb Core Api
    
    * 2 was created Class Library with DAL architecture for entity ProductItem (#1)
    
    * 3 was done settings Program.cs for migration to DB SQLEXPRESS
    
    * 4 was created Db Sql via migration of ProductContext
    
    * 5 was created ProductController with connection to IUnitOfWork
    
    * 6 was done general updates ProductConetxt
    
    * 7 general updates
    
    * release v1.0.0 BLL (#2)
    
    * 2 was created Class Library with DAL architecture for entity ProductItem
    
    * 3 was extended properties of ProductItem, created Class Library BLL
    
    BLL - Bussines Logic Layer
    
    * 4 was created class StatusCode 200, 201... for infrastructure BLL
    
    * 5 was created class ProductItemDto, ProductItemOrder
    
    * 6 was developed bussiness model class EducationTime
    
    * 7 was done class ProductItemException, EducationTime
    
    class EducationTime calculates time of education depends from education form and engineering type.
    
    * 8 was done class ClientPayProperty and DiscountPropery
    
    it is neccessar for manage calculation of payment in class EducationPayment
    
    * 9 was done class EducationPayment for BLL
    
    EducationPayment calculate final price payement for client that depends from client property.
    
    * 10 was started developing of ProductService of BLL
    
    * 11 was developed method getProductDto, getAllProductsDto in ProductService
    
    * 12 was added project AspNet for execute migration to SqlDb
    
    * 13 was done class ProductService in BLL.
    
    * 8 was done presentation layer
    
    Presentaion layer presence as ProductItemController that treats Http requests.

---
+++ ConsoleAppBLL/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AspNetWeb_NLayer.BLL.DTO;
+using AspNetWeb_NLayer.BLL.Infrastructure;
+using AspNetWeb_NLayer.BLL.Services;
+using AspNetWeb_NLayer.DAL.EF;
+using AspNetWeb_NLayer.DAL.Repositories;
+using Microsoft.EntityFrameworkCore;
+using AspNetWeb_NLayer.BLL.BussinesModels;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("BLL is lunched");
+
+        string DefaultDbConnection = "data source=DESKTOP-9C2MS1P\\SQLEXPRESS; Database=products; Trusted_Connection=TRUE; MultipleActiveResultSets=TRUE; TrustServerCertificate=True;";
+        var options = new DbContextOptionsBuilder<ProductContext>().UseSqlServer(DefaultDbConnection).Options;
+
+        var productContext = new ProductContext(options);
+        var products = productContext.productItems;
+
+        try
+        {
+            var ps = new ProductService(new UnitOfWork(productContext));
+            var item = ps.db.productItems.getItem("c++");
+            Console.WriteLine($"ProductItem: {item?.name} | {item?.typeEngeeniring} | {item?.durationMonth} | {item?.price}");
+
+            var pio = ps.getProductOrder("c++", 
+                new ClientTimeProperty() { EducationForm = "holiday" }, 
+                new ClientPayProperty() { IsInvitedPerson = false, PayMethod = "cash", PayPeriod = "monthly"});
+
+            Console.WriteLine($"ProductOrder: {pio.name} | {pio.typeEngeeniring} | {pio.timeStudy} | {pio.sumPay}");
+        }
+        catch (ProductItemException pex)
+        {
+            Console.WriteLine($"{pex.Message} - {pex.property}");
+        }
+    }
+}
+
+//var services = new ServiceCollection();
+//services.AddDbContext<ProductContext>(options => options.UseSqlServer(DefaultDbConnection));
+//var serviceProvider = services.BuildServiceProvider();
