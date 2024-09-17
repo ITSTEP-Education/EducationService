9650a8c403a91d25fe848854562cdb9efc085dae
Kostiantyn Polishko <polxs_wp31@student.itstep.org>
2024-09-17 10:37:53 +0300

    2 was created template projects for xUnitTest of DAL, BLL, PL layers

--- reactapp_productitem/src/components/ProductClientSections/OptionsEducation.styled.ts
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-
-export const OptionsEducationWrap = styled.div`
-    box-sizing: border-box;
-    border-top: 1px solid #ddd;
-`;
--- reactapp_productitem/src/components/ProductItem/ProductItemData.tsx
+++
@@ -1,8 +0,0 @@
-export const dictNameToRoute: {[key: string]: string} = {
-    "c++": "c%2B%2B", 
-    "c#": "c%23",
-}
-
-export const optionsForm: Array<string> = ["daily", "holiday", "remote"];
-export const optionsPayMethod: Array<string> = ["cash", "credit", "bitcoin"];
-export const optionsPayPeriod: Array<string> = ["full", "yearly", "monthly"];
