9c354b0869943cb6ebd89c1d8310bb7e32ec0062
Kostiantyn Polishko <polxs_wp31@student.itstep.org>
2024-09-18 19:30:35 +0300

    4 was added test for check Equivalent result and expected compex data ProductItem

---
+++ TestDAL/Entities/ProductItemTestData.cs
@@ -0,0 +1,31 @@
+﻿using AspNetWeb_NLayer.DAL.Entities;
+using System.Collections;
+
+namespace AspNetWeb_NLayer.DAL.Tests.Entities
+{
+    public class ProductItemTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] { "c++",
+                new ProductItem() {
+                    id = 1,
+                    name = "c++",
+                    description = "low-level language",
+                    typeEngeeniring = "back-end",
+                    durationMonth = 12,
+                    price = 2000 } };
+
+            yield return new object[] { "css",
+                new ProductItem() {
+                    id = 7,
+                    name = "css",
+                    description = "none",
+                    typeEngeeniring = "front-end",
+                    durationMonth = 4,
+                    price = 500 } };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
--- TestDAL/ProductItemRepositoryTests.cs
+++ TestDAL/ProductItemRepositoryTests.cs
@@ -2,7 +2,9 @@ using AspNetWeb_NLayer.DAL.EF;
 using AspNetWeb_NLayer.DAL.Entities;
 using AspNetWeb_NLayer.DAL.Interfaces;
 using AspNetWeb_NLayer.DAL.Repositories;
+using AspNetWeb_NLayer.DAL.Tests.Entities;
 using Moq;
+using System.Diagnostics.CodeAnalysis;
 
 namespace TestDAL
 {
@@ -15,44 +17,48 @@ namespace TestDAL
             repository = new ProductItemRepository(new ProductContext());
         }
 
-        //get IEnumerable<ProductItem> from SQL dbo.productitems
-        [Fact]
-        public void GetAllItemsReturnIsType()
+        [Fact(DisplayName = "get IEnumerable<ProductItem> from SQL dbo.productitems")]
+        public void GetAllItems_ReturnIEnumerableProductItem()
         {
             var result = repository.getAllItems();
             Assert.IsAssignableFrom<IEnumerable<ProductItem>>(result);
         }
 
-        //get correct type ProductItem from SQL dbo.productitems
-        [Fact]
-        public void GetItemIsType()
+        [Fact(DisplayName = "get correct type ProductItem from SQL dbo.productitems")]
+        public void GetItem_ReturnProductItem()
         {
             var result = repository.getItem("c++");
             Assert.IsType<ProductItem>(result);
         }
 
-        //get ArgumentNullException if argument is null
-        [Fact]
-        public void GetItemArgNull()
+        [Fact(DisplayName = "get ArgumentNullException if argument is null")]
+        public void GetItem_ArgNull_ThrowsArgumentNullException()
         {
             Assert.Throws<ArgumentNullException>(() => repository.getItem(null));
         }
 
-        //get ArgumentNullException if argument is string.Empty
-        [Fact]
-        public void GetItemArgStringEmpty()
+        [Fact(DisplayName = "get ArgumentNullException if argument is string.Empty")]
+        public void GetItem_ArgStringEmpty_ThrowsArgumentNullException()
         {
             Assert.Throws<ArgumentNullException>(() => repository.getItem(string.Empty));
         }
 
-        //get correct result of ProductItem depend from name of product
-        [Theory]
+        [Theory(DisplayName = "get correct result of price ProductItem depend from name of product")]
         [InlineData("c++", 2000)]
         [InlineData("css", 500)]
-        public void GetItemCorrectResult(string name, int expected)
+        public void GetItem_ArgName_ReturnCorrectPrice(string name, int expected)
         {
             var result = repository.getItem(name);
             Assert.StrictEqual(expected, result?.price);
+        }
+
+        [Theory (DisplayName = "get correct result of ProductItem depend from name of product")]
+        //[MemberData(nameof(TestProductItem))]
+        [ClassData(typeof(ProductItemTestData))]
+        public void GetItem_ArgName_ReturnCorrectProductItem(string name, ProductItem expected)
+        {
+            var result = repository.getItem(name);
+            Assert.Equivalent(expected, result);
         }
     }
 }
