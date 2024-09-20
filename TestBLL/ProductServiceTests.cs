87740909d97511f6e70107c713302daa7dcfb311
KostiantynPolishko <polxs_wp31@student.itstep.org>
2024-09-20 10:53:14 +0300

    8 add description of several methods for tests BLL

---
+++ TestBLL/ProductServiceTests.cs
@@ -0,0 +1,29 @@
+namespace TestBLL
+{
+    public class ProductServiceTests
+    {
+        [Fact(DisplayName = "1 get ProductItem by Name and throw ProductItemException for null")]
+        public void GetProductItem_ByName_ReturnException()
+        {
+
+        }
+
+        [Fact(DisplayName = "2 get list of ProductItems from ")]
+        public void GetProductsDto_FromProductItem_UseMap_ReturnCorrectResult()
+        {
+
+        }
+
+        [Fact(DisplayName = "")]
+        public void GetProductsDto_FromProductItem_ReturnException()
+        {
+
+        }
+
+        [Fact(DisplayName = "")]
+        public void GetProductOrder_WithBussinesModels_ReturnCorrectResult()
+        {
+
+        }
+    }
+}
