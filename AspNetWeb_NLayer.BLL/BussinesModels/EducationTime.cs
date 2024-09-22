8a9441e6db36edbae8f595895ccf82e4db41ad69
Kostiantyn Polishko <167745085+KostiantynPolishko2@users.noreply.github.com>
2024-09-22 14:28:46 +0300

    Dev multy layers (#7)
    * 2 was created default project AspNetWeb Core Api
    
    * 2 was created Class Library with DAL architecture for entity ProductItem (#1)
    
    * 3 was done settings Program.cs for migration to DB SQLEXPRESS
    
    * 4 was created Db Sql via migration of ProductContext
    
    * 5 was created ProductController with connection to IUnitOfWork
    
    * 6 was done general updates ProductConetxt
    
    * 7 general updates
    
    * release v1.0.0 BLL (#2)
    
    * 2 was created Class Library with DAL architecture for entity ProductItem
    
    * 3 was extended properties of ProductItem, created Class Library BLL
    
    BLL - Bussines Logic Layer
    
    * 4 was created class StatusCode 200, 201... for infrastructure BLL
    
    * 5 was created class ProductItemDto, ProductItemOrder
    
    * 6 was developed bussiness model class EducationTime
    
    * 7 was done class ProductItemException, EducationTime
    
    class EducationTime calculates time of education depends from education form and engineering type.
    
    * 8 was done class ClientPayProperty and DiscountPropery
    
    it is neccessar for manage calculation of payment in class EducationPayment
    
    * 9 was done class EducationPayment for BLL
    
    EducationPayment calculate final price payement for client that depends from client property.
    
    * 10 was started developing of ProductService of BLL
    
    * 11 was developed method getProductDto, getAllProductsDto in ProductService
    
    * 12 was added project AspNet for execute migration to SqlDb
    
    * 13 was done class ProductService in BLL.
    
    * 8 was done presentation layer
    
    Presentaion layer presence as ProductItemController that treats Http requests.
    
    * 9 modified bussines models EducationTime and extended DiscountProperty

---
+++ AspNetWeb_NLayer.BLL/BussinesModels/EducationTime.cs
@@ -0,0 +1,37 @@
+﻿using AspNetWeb_NLayer.BLL.Infrastructure;
+
+namespace AspNetWeb_NLayer.BLL.BussinesModels
+{
+    public class EducationTime : Dictionary<string, int>
+    {
+        public ClientTimeProperty cltTimeProps { get; } = null!;
+
+        public EducationTime(ClientTimeProperty cltTimeProps) : base(new Dictionary<string, int>(){ { "front-end", 2 }, { "back-end", 4 } })
+        {
+            this.cltTimeProps = cltTimeProps;
+        }
+
+        public int getTimeEducation(int timeDurationMonth)
+        {
+            if (cltTimeProps.EducationForm != "daily" && cltTimeProps.EducationForm != "holiday")
+                throw new ProductItemException("absent form education", cltTimeProps.EducationForm);
+
+            if (cltTimeProps.EducationForm == "daily")
+            {
+                return timeDurationMonth;
+            }
+            else if (cltTimeProps.EducationForm == "holiday")
+            {
+                return timeDurationMonth + this[cltTimeProps.EngineerType];
+            }
+            else if (cltTimeProps.EducationForm == "remote")
+            {
+                return (int)((timeDurationMonth + this[cltTimeProps.EngineerType]) * 1.5);
+            }
+            else
+            {
+                throw new ProductItemException("absent engineer type", cltTimeProps.EngineerType);
+            }
+        }
+    }
+}
