7c4056febfb5fbb43fdc87b687d697bd022c46d0
KostiantynPolishko <polxs_wp31@student.itstep.org>
2024-09-22 15:17:51 +0300

    2 was added description of controller to SwaggerUI

--- AspNetWeb_Product/Program.cs
+++ AspNetWeb_Product/Program.cs
@@ -4,6 +4,7 @@ using AspNetWeb_NLayer.DAL.EF;
 using AspNetWeb_NLayer.DAL.Interfaces;
 using AspNetWeb_NLayer.DAL.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,7 +16,26 @@ builder.Services.AddScoped<IProductService, ProductService>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1.0.0", new OpenApiInfo
+    {
+        Version = "v1.0.0",
+        Title = "ProductItem Api",
+        Description = "Api request to get ProductItem and convert it to ProductOrder",
+        Contact = new OpenApiContact
+        {
+            Name = "KostiantynPolishko",
+            Email = "polxs_wp31@student.itstep.org",
+            Url = new Uri("https://github.com/KostiantynPolishko2/AspNetWeb_NLayer/tree/dev_MultyLayers")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "ApiITSTEP",
+            Url = new Uri("https://mystat.itstep.org/")
+        }
+    });
+});
 
 var app = builder.Build();
 
@@ -23,7 +43,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(config =>
+    {
+        config.SwaggerEndpoint("/swagger/v1.0.0/swagger.json", "v1.0.0");
+    });
 }
 
 app.UseHttpsRedirection();
